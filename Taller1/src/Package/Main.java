c0956fd4a2d74715237df3f41c2989ba9c1751a2
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-06-08 19:27:10 -0500

    adding the function suma()

--- Taller1/src/Package/Main.java
+++ Taller1/src/Package/Main.java
@@ -1,7 +1,16 @@
 package Package;
 
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void suma(int a, int b, int c) {
+        int resultado = a + b + c;
+        System.out.println(resultado);
 
     }
+    public static void main(String[] args) {
+    suma(5,10,20);
+    }
+
+
 }
