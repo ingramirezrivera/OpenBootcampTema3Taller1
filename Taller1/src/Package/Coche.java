304b5328e0381e23eb73ca3257db4b8292741886
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-06-08 19:33:25 -0500

    Adding the class Coche

--- Taller1/src/Package/Coche.java
+++ Taller1/src/Package/Coche.java
@@ -1,4 +1,16 @@
 package Package;
 
 public class Coche {
+    //properties
+    int nuemroPuertas = 4;
+    //constructor
+    Coche(int nuemroPuertas){
+        this.nuemroPuertas = nuemroPuertas;
+    }
+    //method
+    public void setPuerta(){
+        this.nuemroPuertas += 1;
+    }
+
+
 }
--- Taller1/src/Package/Main.java
+++ Taller1/src/Package/Main.java
@@ -10,6 +10,11 @@ public class Main {
     }
     public static void main(String[] args) {
     suma(5,10,20);
+
+    Coche miCoche = new Coche(3);
+        System.out.println("El numero de puertas de mi coche es: " + miCoche.nuemroPuertas);
+        miCoche.setPuerta();
+        System.out.println("Le sumamos una purta al coche" + miCoche.nuemroPuertas);
     }
 
 
